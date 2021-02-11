5c9749cc989a45df26957b2dda346f0bc794ad86
Stefano Paulo <stefano.souza@hapvida.com.br>
2021-02-11 17:20:18 -0300

    Department entity class

---
+++ demo-dao-jdbc/src/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Department obj = new Department(1, "Books");
+		
+		System.out.println(obj);
+		
+	}
+
+}

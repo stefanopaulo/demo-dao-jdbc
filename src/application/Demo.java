001d4dc50285a489dd5e334d21863582d68d444c
Stefano Paulo <stefanopaulo47@gmail.com>
2025-12-11 09:54:24 -0300

    Entidade Seller

--- src/application/Demo.java
+++ src/application/Demo.java
@@ -1,10 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Demo {
     public static void main(String[] args) {
         Department department = new Department(1, "Books");
         System.out.println(department);
+
+        Seller seller = new Seller(1, "Alex Gray", "alex@gmail.com", LocalDate.parse("1985-04-22"), 4000.0, department);
+        System.out.println(seller);
     }
 }
